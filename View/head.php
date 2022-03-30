d5eff369cf84316b2d230630007ac3b4c954057a
Dobrosi Gergo <dobrosi.gergo13@gmail.com>
2022-03-31 01:02:55 +0200

    asd

--- View/head.php
+++ View/head.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="/css/main.css?=<?= rand(1, 12000) ?>">
+<link rel="stylesheet" href="/files/css/main.css?=<?= rand(1, 12000) ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-<link rel="stylesheet" href="/css/explore.css?=<?= rand(1, 12000) ?>">
-<script src="../js/explore.js"></script>
+<link rel="stylesheet" href="/files/css/explore.css?=<?= rand(1, 12000) ?>">
+<script src="/files/js/explore.js"></script>
 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 <link rel="stylesheet" href="/css/show.css?=<?= rand(1, 12000) ?>">
 <link rel="stylesheet" href="../css/login.css?=<?= rand(1, 12000) ?>">
 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-<script src="../js/login.js"></script>
+<script src="/files/js/login.js"></script>
