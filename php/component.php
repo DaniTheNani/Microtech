4712e9e0feb4454eade750adb3f26e6f51a83a94
Dani <happy125@citromail.hu>
2022-03-31 00:58:04 +0200

    hivatkozás beágyazása

--- php/component.php
+++ php/component.php
@@ -19,10 +19,6 @@ session_start();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/main.css?=<?= rand(1, 12000) ?>">
         <title>Micro Tech - Alkatrészek</title>
     </head>
 
--- php/explore.php
+++ php/explore.php
@@ -16,7 +16,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/explore.css?=<?= rand(1, 12000) ?>">
+
     <title>Micro Tech - Explore</title>
 </head>
 
@@ -36,12 +36,8 @@ session_start();
             <a href="administration.php"><span>Belépés mint Adminisztrátor</span></a>
             </div>';
         }
-
         ?>
-
     </div>
-
-    <script src="../js/explore.js"></script>
 </body>
 
 </html>
